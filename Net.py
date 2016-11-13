206d0cf8be72ecc1779df60ffde038608717fa93
Pooneet Thaper <pooneetthaper@gmail.com>
2016-11-13 01:44:10 -0500

    encorporated game code into net

--- Net.py
+++ Net.py
@@ -6,22 +6,73 @@ def nonlin(x, deriv=False):
     return 1/(1+np.exp(-x))
 
 
-board = [  [ 0, 0, 0, 0, 0, 0, 0],
-                    [ 0, 0, 0, 0, 0, 0, 0],
-                    [ 0, 0, 0, 0, 0, 0, 0],
-                    [-1, 1,-1,-1, 1, 1, 1],
-                    [ 1, 1, 1,-1, 1,-1,-1],
-                    [ 1, 1,-1,-1, 1, 1, 1]]
-
+board = []
+colTracker = []
 players=[]
 
+def addPiece(column, p):
+    count=0
+    winner=0
+    if(colTracker[column]<0):
+        winner=p*-1
+        print winner
+        return winner
+    else:
+        board[colTracker[column]][column]=p
+        colTracker[column]-=1
+        for i in range(0,6): # check column win
+    		if((count > 0) and (board[i][column] != p)): # reset counter if not consecutive
+    			count = 0
+    		if(board[i][column] == p):
+    			count += 1
+
+    		if count == 4: # WIN CONDITION MET
+    			winner = p
+    			print winner,' won'
+    			return winner
+    	count = 0 # reset
+        print winner
+    	return winner
+        #Win Check
+    '''
+	count = 0
+	for i in range(5,-1,-1): # iterate rows to add piece
+
+		if(board[i][column] == 0): # checking for empty row to place
+			board[i][column] = p
+
+			for j in range(0, 7): # check row win
+				if((count > 0) and (board[i][j] != p)): # reset counter if not consecutive
+					count = 0
+				if(board[i][j] == p):
+					count += 1
+
+				if(count == 4): # WIN CONDITION MET
+					winner = p
+					print winner,' won'
+					return
+			break
+		elif(i == 0): # LOSE CONDITION MET
+			winner = -1*p
+			return
+	count = 0 # reset
+	# check win conditions
+	for i in range(0,6): # check column win
+		if((count > 0) and (board[i][column] != p)): # reset counter if not consecutive
+			count = 0
+		if(board[i][column] == p):
+			count += 1
+
+		if count == 4: # WIN CONDITION MET
+			winner = p
+			print winner,' won'
+			return
+	count = 0 # reset
+	return
+    '''
 
 def runPlayer(playerNum):
-    b=[]
-    for row in board:
-        for k in row:
-            b.append(k)
-    out=(nonlin(np.dot(b, players[playerNum])))
+    out=(nonlin(np.dot(flatten(6,7,board), players[playerNum])))
     m=out[0]
     index=0
     for x in range(1,7):
@@ -30,14 +81,45 @@ def runPlayer(playerNum):
             index=x
     return index
 
-def start():
+def play(playerA,playerB):
+    player=1
+    w=0
+    while(w==0):
+        for p in range(6):
+        	print board[p]
+        if(player==1):
+            w=addPiece(runPlayer(playerA), player)
+        else:
+            w=addPiece(runPlayer(playerA), player)
+    	player *= -1
+
+    if(w==1):
+        return playerA
+    else:
+        return playerB
+
+def init():
     numIn=42
     numOut=7
     numPlayers=8
     np.random.seed(0)
+    for i in range(0,6): # rows
+    	board.append([])
+    	for j in range(0,7): # columns
+    		board[i].append(0)
+    for i in range(7):
+        colTracker.append(5)
     for k in range(numPlayers):
         players.append(2*np.random.random((numIn,numOut))-1)
-    print(runPlayer(4))
+    print play(1,7),"won"
+
+
+def flatten(x,y,arr):
+    out=[]
+    for a in range(x):
+        for b in range(y):
+            out.append(board[a][b])
+    return out
 
 if __name__ == '__main__':
-    start()
+    init()
