95dbdc424f4684c2322f3f6a4cf729eb458700b0
Pooneet Thaper <pooneetthaper@gmail.com>
2016-12-29 22:11:18 -0500

    results after some training, new larger nets need much more training, still spamming a single row

--- Genetic Model/Net.py
+++ Genetic Model/Net.py
@@ -2,9 +2,20 @@ import numpy as np
 import csv
 import time
 
-def nonlin(x, deriv=False):
-    if(deriv==True):
-        return (x*(1-x))
+'''
+    Hello, Welcome to Connect4I
+    This project uses neural nets and genetic algorithms to ATTEMPT to teach the computer to play Connect$
+    In order to run this project, simply navigate to the folder with this file and use the command line command:
+    "python Net.py"
+
+    It will show you the board and prompt you when it is your turn. You will be playing as playerB
+    and your peices on the board will be "-1" (because we wanted the nets to recognize you as the enemy)
+    If you would like to see the computer play againsts itself, please go down to the demo function and
+    change the default value of the manual parameter to 0.
+'''
+
+def nonlin(x):
+    #this function is used as the activation function of the neurons (sigmoid)
     return 1/(1+np.exp(-x))
 
 
@@ -13,6 +24,8 @@ colTracker = []
 players=[]
 
 def addPiece(column, p):
+    #the addpeice checks to see if the game has been won or lost
+    #note an illegal move (ex: a move in a full row) is an automatic loss
 	count=0
 	winner=0
 	if(colTracker[column]<0): # LOSE CONDITION MET
@@ -107,60 +120,60 @@ def addPiece(column, p):
 				winner = p
 				return winner
 		count = 0 # reset
-
 		#print 'Current Winner ',winner
 		return winner
 
 def runPlayer(playerNum,playerVal):
+    #Getting the weights of the player in question
     currentPlayer=players[playerNum];
+    #Getting the output by using matrix multiplication on a flattened version of the board
     out1=(nonlin(np.dot(flatten(6,7,board,playerVal), currentPlayer[0])))
     out2=(nonlin(np.dot(out1,currentPlayer[1])))
     out=(nonlin(np.dot(out2,currentPlayer[2])))
     #print out
+    #Finding the max of the seven outputs to decide on the next move
     m=out[0]
     index=0
     for x in range(1,7):
         if(out[x]>m):
             m=out[x]
             index=x
+    #Returning move
     return index
 
 def play(playerA,playerB):
+    #controls the game between two players
     reset()
     player=1
     w=0
     out=-1
+    #while there is no winner, switches between the players
     while(w==0):
-        #for p in range(6):
-        #	print board[p][0],"\t",board[p][1],"\t",board[p][2],"\t",board[p][3],"\t",board[p][4],"\t",board[p][5],"\t",board[p][6]
-        #print "\n"
         if(player==1):
             w=addPiece(runPlayer(playerA,1), player)
         else:
             w=addPiece(runPlayer(playerA,-1), player)
     	player *= -1
-
-
     if(w==1):
         out=playerA
     else:
         out=playerB
-
-    #print out,"won"
-
     return out
 
 def train():
     for x in range(1000000):
+        #tournament is run to have the eight players compete and return the two winners
         k=tournament()
+        #gen is used to create the next generation from the winners from the tournament
         gen(k[0],k[1])
         if (x%100==0):
             print(x)
         if(x%1000==999):
-            out=flattenPlayer(0)
+            #periodically, the results are printed out and saved to a CSV
+            out=flattenPlayer(7)
             csvOut=[]
             for i in range(len(out)):
-                csvOut.append(str(out[i]))
+                csvOut.append(str(float(out[i])))
             with open("out2.csv","w+") as f:
                 spamwriter = csv.writer(f, delimiter=',',
                                     quotechar='|', quoting=csv.QUOTE_MINIMAL)
@@ -171,6 +184,8 @@ def train():
 
 
 def gen(playerA,playerB):
+        #given the two starting players, gen creates 6 players based of of the original 2
+        #random crossbreeding and mutations are done for variations
     	d = []
     	d.append(flattenPlayer(playerA))
     	d.append(flattenPlayer(playerB))
@@ -181,8 +196,7 @@ def gen(playerA,playerB):
     		if(a[l] > 0.5):
     			temp = d[2][l]
     			d[2][l] = d[3][l]
-    			d[3][l] = temp*(np.random.random(1)[0]/5 +0.9)
-                #temp is multiplied by a number between 0.9 and 1.1 for randomness
+    			d[3][l] = temp*(2*np.random.random(1)-1)
         for l in range(len(d)):
             d.append(d[l][:])
         for j in range(2):
@@ -191,25 +205,30 @@ def gen(playerA,playerB):
     			if(a[l] > 0.5):
     				temp = d[len(d)/2+j][l]
     				d[len(d)/2+j][l]=d[len(d)-j-1][l]
-    				d[len(d)-j-1][l]=temp*(np.random.random(1)[0]/5 +0.9)
-                    #temp is multiplied by a number between 0.9 and 1.1 for randomness
+    				d[len(d)-j-1][l]=temp*(2*np.random.random(1)-1)
         for l in range(len(players)):
             players[l]=deflattenPlayer(d[l])
 
 def flattenPlayer(playerNum):
+    #flattens the three layers of the players into one long array
     input=[]
+
     curr=flatten(42,20,players[playerNum][0])
     for k in curr:
         input.append(k)
+
     curr=flatten(20,20,players[playerNum][1])
     for k in curr:
         input.append(k)
+
     curr=flatten(20,7,players[playerNum][1])
     for k in curr:
         input.append(k)
+
     return input
 
 def deflattenPlayer(arr):
+    #reconstructs the players from a flattened player
     out=[]
 
     temp=[]
@@ -233,6 +252,8 @@ def deflattenPlayer(arr):
     return out;
 
 def deflatten(x,y,arr):
+    #turns a single dimensional array into two dimensional array of dimensions x and y
+    #(reverses flatten)
     out=[]
     for a in range(x):
         o=[]
@@ -242,6 +263,8 @@ def deflatten(x,y,arr):
     return out
 
 def flatten(x,y,arr,pval=1):
+    #turns a turns a two dimensional array of dimensions x and y into a
+    #single dimensional array for more efficient operations
     out=[]
     for a in range(x):
         for b in range(y):
@@ -268,7 +291,7 @@ def reset():
         for col in range(7):
             board[row][col]=0
 
-def demo(playerA,playerB):
+def demo(playerA,playerB,manual=1):
     reset()
     player=1
     w=0
@@ -284,12 +307,15 @@ def demo(playerA,playerB):
         if(player==1):
             w=addPiece(runPlayer(playerA,1), player)
         else:
-
-            print "Choose your column (1-7)"
-            r=raw_input()
-            w=addPiece(int(r)-1, player)
-
-            #w=addPiece(runPlayer(playerB,-1), player)
+            if(manual==1):
+                print "Choose your column (1-7)"
+                r=raw_input()
+                w=addPiece(int(r)-1, player)
+                print "\n"
+            else:
+                #To watch the computer play itself,
+                #change manual default in function declaration to 0
+                w=addPiece(runPlayer(playerB,-1), player)
     	player *= -1
 
 
@@ -302,32 +328,41 @@ def demo(playerA,playerB):
         print "playerB won"
 
 def run():
-    train()
-    #demo(7,7)
+    #Uncomment train in order to train
+    #train()
+    #Uncomment demo in order to run the model
+    demo(7,7)
     print('done')
 
 def init():
+    #initial parameters
     numIn=42
     numOut=7
     numPlayers=8
+    #setting up board
     for i in range(0,6): # rows
     	board.append([])
     	for j in range(0,7): # columns
     		board[i].append(0)
+    #setting up tracking for each column (for illegal move checking)
     for i in range(7):
         colTracker.append(5)
-    for k in range(numPlayers-1):# Use numPlayers-1 when loading from csv
+    #creating random weights for neural nets
+    for k in range(numPlayers-1):
+        # numPlayers-1 are created while the last one (pretrained) is loaded from the csv
         p=[]
         p.append(2*np.random.random((numIn,20))-1)
         p.append(2*np.random.random((20,20))-1)
         p.append(2*np.random.random((20,numOut))-1)
-
         players.append(p);
+        #players are stored in the player global array
+
     r=[]
     with open("out2.csv","r") as f:
         spamreader = csv.reader(f, delimiter=',', quotechar='|')
         for row in spamreader:
             for item in row:
+                #print item;
                 r.append(float(item))
     players.append(deflattenPlayer(r))
 
